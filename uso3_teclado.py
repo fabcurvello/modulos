7f0251b721763a454211b27567ef150fd49f1e9c
fabriciocurvello <tiofab@gmail.com>
2023-02-03 21:19:35 -0300

    Projeto para AULAS de INICIACAO em PYTHON

---
+++ uso3_teclado.py
@@ -0,0 +1,9 @@
+from teclado import *
+
+nome = ler_str("Qual o seu nome? ")
+idade = ler_int("Qual a sua idade? ")
+salario = ler_float("Qual o seu salário? ")
+
+print(f"\nNome inserido: {nome}")
+print(f"Idade inserida: {idade}")
+print(f"Salário inserido: R$ {salario}")
