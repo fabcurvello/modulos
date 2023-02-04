88e4278068d7a1271ba5a3c02d25a1a812a014cd
fabriciocurvello <tiofab@gmail.com>
2023-02-03 21:15:48 -0300

    Projeto para AULAS de INICIACAO em PYTHON

--- teclado.py
+++ teclado.py
@@ -1,12 +1,16 @@
 def ler_str(mensagem):
-    return input(mensagem)
+    resposta = input(mensagem)
+    while (resposta == ""):
+        print("ERRO: Por favor, digite a sua resposta!")
+        resposta = input(mensagem)
+    return resposta
 
 
 def ler_int(mensagem):
     try:
         numero = int(ler_str(mensagem))
     except:
-        print("ERRO: O valor inserido precisa ser um número inteiro")
+        print("ERRO: O valor inserido precisa ser um número inteiro!")
         numero = ler_int(mensagem)
     finally:
         return numero
@@ -16,17 +20,9 @@ def ler_float(mensagem):
     try:
         numero = float(ler_str(mensagem))
     except:
-        print("ERRO: O valor inserido precisa ser um número real (float)")
+        print("ERRO: O valor inserido precisa ser um número real (float)!")
         numero = ler_float(mensagem)
     finally:
         return numero
 
 
-nome = ler_str("Qual o seu nome? ")
-print(nome)
-
-idade = ler_int("Qual a sua idade? ")
-print(idade)
-
-salario = ler_float("Qual o seu salário? ")
-print(salario)
---
+++ uso1_teclado.py
@@ -0,0 +1,9 @@
+import teclado
+
+nome = teclado.ler_str("Qual o seu nome? ")
+idade = teclado.ler_int("Qual a sua idade? ")
+salario = teclado.ler_float("Qual o seu salário? ")
+
+print(f"\nNome inserido: {nome}")
+print(f"Idade inserida: {idade}")
+print(f"Salário inserido: R$ {salario}")
